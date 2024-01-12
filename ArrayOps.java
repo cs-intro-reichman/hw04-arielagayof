de285122b625bd8d6ec49e3bfd0833ab082611e7
Ariel Agayof <arielagayof@Ariels-MacBook-Air.local>
2024-01-12 20:45:18 +0200

    upload

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,98 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
-    }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int[] a = {1, 3, 3, 4};
+        System.out.println(isSorted(a));
     }
 
+     public static int findMissingInt (int [] array) {
+        int arrayLength = array.length;
+        int arrSum = 0;
+        int expectedSum = 0;
+        for (int i = 0 ; i <= arrayLength ; i++){
+            expectedSum = expectedSum + i;
+        }
+        for (int j = 0 ; j < arrayLength ; j++){
+            arrSum = arrSum + array[j];
+        }
+        int missintInt = expectedSum - arrSum; 
+        return missintInt;
+    }
+
+    
+
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int max = array[0];
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        int max2 = min;
+        for (int i = 1; i < array.length; i++) {
+            if ((array[i] > max2) && (array[i] != max)) {
+                max2 = array[i];
+            }
+        }
+        return max2;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
+        boolean sameElements1 = sameElements(array1, array2);
+        boolean sameElements2 = sameElements(array2,array1);
+        if (sameElements1 && sameElements2){
+            return true;
+        }
+        return false;
+    }
+    public  static boolean sameElements(int [] array1,int [] array2){
+        boolean sameNums = false;
+
+        for (int i = 0 ; i < array1.length; i++){
+            sameNums=false;
+            for (int j = 0; j < array2.length; j++){
+                if(array1[i] == array2[j]){
+                    sameNums = true;
+                    break;
+                }
+            }
+
+            if(!sameNums){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean isSorted(int [] array) {
+        if (isDecreasing(array) || isIncreasing(array)) {
+            return true;
+        }
         return false;
     }
 
-    public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+    public static boolean isIncreasing(int [] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isDecreasing(int [] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[i - 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,71 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        int [] a = allIndexOf("Hello world",'d');
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+   public static String capVowelsLowRest (String string) {
+    
+        String newWord = "";
+        for (int i = 0; i < string.length(); i++){
+            char currentChar = string.charAt(i);
+            if (currentChar == 'a' ||currentChar ==  'e' ||currentChar ==  'i' ||currentChar ==  'o' ||currentChar ==  'u'){
+                newWord += (char)(currentChar - 32);
+            }
+            else if (currentChar >= 'A' && currentChar <= 'Z' && (currentChar != 'A' && currentChar != 'E' && currentChar != 'I' && currentChar != 'O' && currentChar != 'U')) {
+                newWord += (char)(currentChar + 32);
+            }
+            else {
+                newWord += currentChar;
+            }
+        }
+        return newWord;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
-    }
+        String res = "";
 
+        for (int i = 0; i < string.length() ; i++) {
+            while (string.charAt(i) == ' ') {
+                i= i+1;
+                if (res != ""){
+                  if (string.charAt(i) >= 'a' && string.charAt(i) <= 'z') {
+                    res += (char)(string.charAt(i) - 32);
+                    i++;
+                  }
+                  else if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z') {
+                    res += string.charAt(i);
+                    i++;
+                  }
+                }
+            }
+            if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z') {
+                res += (char)(string.charAt(i) + 32);
+            }
+            else {
+                res += string.charAt(i);
+            }
+        }
+                return res;
+    }
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                count ++;
+            }
+        }
+        int [] allIndex = new int[count];
+        int index = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                allIndex[index] = i;
+                index++;
+            }
+        }
+        return allIndex;
     }
 }
